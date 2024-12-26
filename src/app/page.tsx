0ca30859e6ee02793030aaa2ef9c116a85a084f1
GJA <792002710@qq.com>
2024-12-26 18:48:13 +0800

    build(dependencies): 添加 Prisma 并移除无用代码
    - 在 package.json 中添加 @prisma/client 和 prisma 依赖
    - 更新 pnpm-lock.yaml，包含新增的 Prisma 相关包
    - 移除 src/app/page.tsx 中的无用代码，为使用 Prisma 做准备

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import { addTodo, getTodos } from "@/actions";
-import ClientButton from "@/components/client-button";
-import SubmitButton from "@/components/submit-button";
-import SubmitForm from "@/components/submit-form";
+import "@/db";
 
-export default async function Page() {
-  const todos = await getTodos();
-  return (
-    <div className="p-10">
-      <SubmitForm/>
-      <ul className="leading-8 mt-4">
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
-  );
+export default function Page() {
+  return <div></div>;
 }
