ef02a0af889c5b32fef9492a6c9654244bbf7388
GJA <792002710@qq.com>
2024-12-26 18:02:15 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { addTodo, getTodos } from "@/actions";
+import ClientButton from "@/components/client-button";
+import SubmitButton from "@/components/submit-button";
+import SubmitForm from "@/components/submit-form";
+
+export default async function Page() {
+  const todos = await getTodos();
+  return (
+    <div className="p-10">
+      <SubmitForm/>
+      <ul className="leading-8 mt-4">
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
