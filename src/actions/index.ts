b96b567a27fbbe02072c15cd8f1d1532b7165561
GJA <792002710@qq.com>
2024-12-30 10:51:34 +0800

    feat(snippets): 实现代码片段创建功能并添加表单验证
    - 在 actions/index.ts 中添加 createSnippet 函数，用于创建新的代码片段
    - 在 snippets/new/page.tsx 中实现表单提交逻辑，使用 createSnippet 函数
    - 添加表单验证，确保标题和代码内容至少为 3 个字符
    - 在客户端页面中显示表单验证错误信息

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,3 +17,44 @@ export async function editSnippet(id: number, code: string) {
   });
   redirect("/snippets/" + id);
 }
+
+export async function createSnippet(
+  prevState: { message: string },
+  formData: FormData
+) {
+  "use server";
+  try {
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be at least 3 characters",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 3) {
+      return {
+        message: "Code must be at least 3 characters",
+      };
+    }
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong",
+      };
+    }
+  }
+
+  redirect("/");
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,25 +1,15 @@
+"use client";
 import React from "react";
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+import { createSnippet } from "@/actions";
+import { useFormState } from "react-dom";
 
+const initState = {
+  message: "",
+};
 export default function page() {
-  async function createSnippet(formData: FormData) {
-    "use server";
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    console.log("formData", formData);
-
-    await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    redirect("/");
-  }
+  const [state, formAction] = useFormState(createSnippet, initState);
   return (
-    <form action={createSnippet}>
+    <form action={formAction}>
       <h3 className="font-bold m-3 text-center">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -46,6 +36,11 @@ export default function page() {
             id="code"
           />
         </div>
+        {state.message && (
+          <div className="my-2 p-2 bg-red-200 rounded border-red-500">
+            {state.message}
+          </div>
+        )}
         <button className="bg-blue-200 rounded p-2" type="submit">
           Create
         </button>
